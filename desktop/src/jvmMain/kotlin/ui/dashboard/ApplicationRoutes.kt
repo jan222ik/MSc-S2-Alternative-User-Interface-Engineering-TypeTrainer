f2a9e51d5403340f3e5685c057f8172bd0aec214
Janik Mayr <jan.222.ik@gmail.com>
2021-02-18 17:24:48 +0100

    Added app specific main router

---
+++ desktop/src/jvmMain/kotlin/ui/dashboard/ApplicationRoutes.kt
@@ -0,0 +1,14 @@
+package ui.dashboard
+
+import ui.util.i18n.RequiresTranslationI18N
+import ui.util.i18n.KeyI18N
+import ui.util.i18n.i18n
+
+sealed class ApplicationRoutes(val title: KeyI18N) {
+    object Dashboard : ApplicationRoutes(title = RequiresTranslationI18N("Dashboard"))
+    object Compose : ApplicationRoutes(title = RequiresTranslationI18N("Create new Dive Entry"))
+    object Settings : ApplicationRoutes(title = i18n.str.navigation.settings)
+    object Pictures : ApplicationRoutes(title = RequiresTranslationI18N("Pictures"))
+    object Logbook : ApplicationRoutes(title = RequiresTranslationI18N("Logbook"))
+    object ExerciseSelection : ApplicationRoutes(title = RequiresTranslationI18N("Exercise Selection"))
+}
