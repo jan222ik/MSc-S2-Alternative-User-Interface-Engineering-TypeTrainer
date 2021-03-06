47847b3996ca8f2ca2f4d71d9e4fc3d89b0da45a
Janik Mayr <jan.222.ik@gmail.com>
2021-03-06 22:01:58 +0100

    Update debuggable practice screen

--- desktop/src/jvmMain/kotlin/ui/exercise/practice/PracticeScreen.kt
+++ desktop/src/jvmMain/kotlin/ui/exercise/practice/PracticeScreen.kt
@@ -1,11 +1,13 @@
+@file:Suppress("FunctionName")
+
 package ui.exercise.practice
 
 import TypeTrainerTheme
+import androidx.compose.desktop.AppWindow
 import androidx.compose.desktop.LocalAppWindow
 import androidx.compose.desktop.Window
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,6 +25,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import textgen.generators.impl.RandomKnownWordGenerator
 import ui.components.progress.practice.CountDownProgressBar
@@ -52,7 +55,15 @@ private fun PracticeScreenContent(intend: IPracticeIntend) {
     val localWindow = LocalAppWindow.current
     LaunchedEffect(intend) {
         if (System.getProperty("debug") == "true") {
-            PracticeScreenDebugable(intend as IDebugPracticeIntend, IntOffset(localWindow.x, localWindow.y), localWindow.width)
+            PracticeScreenDebuggable(
+                intend = intend as IDebugPracticeIntend,
+                parentWindowLocation = IntOffset(localWindow.x, localWindow.y),
+                parentWindowWidth = localWindow.width,
+                parentWindowHeight = localWindow.height,
+                windowClose = {
+                    localWindow.events.onClose = it
+                }
+            )
         }
     }
     val max = intend.typingOptions.durationMillis.div(1000).toFloat()
@@ -78,103 +89,129 @@ private fun PracticeScreenContent(intend: IPracticeIntend) {
 
 
 @OptIn(ExperimentalFoundationApi::class)
-fun PracticeScreenDebugable(intend: IDebugPracticeIntend, parentWindowLocation: IntOffset, parentWindowWidth: Int) {
-    Window(
-        title = "PracticeScreenDebugable",
+fun PracticeScreenDebuggable(
+    intend: IDebugPracticeIntend,
+    parentWindowLocation: IntOffset,
+    parentWindowWidth: Int,
+    parentWindowHeight: Int,
+    windowClose: (() -> Unit) -> Unit
+) {
+    AppWindow(
+        title = "PracticeScreenDebuggable",
         location = parentWindowLocation.copy(x = parentWindowLocation.x.plus(parentWindowWidth)),
         //TODO Check that it is still on screen
-        centered = false
-    ) {
+        centered = false,
+        size = IntSize(width = 400, height = parentWindowHeight)
+    ).show {
+        val current = LocalAppWindow.current
+        windowClose.invoke {
+            try {
+                current.close()
+            } catch (e: IllegalStateException) {
+
+            }
+        }
         TypeTrainerTheme {
             Surface(
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colors.surface
             ) {
-                LazyColumn(modifier = Modifier.padding(8.dp).fillMaxSize()) {
+                LazyColumn(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
                     stickyHeader {
-                        Text(text = "Options:")
-                    }
-                    item {
-                        val list = listOf(
-                            "DurationMillis:" to intend.typingOptions.durationMillis,
-                            "Type:" to intend.typingOptions.type
-                        )
-                        BaseDashboardCard {
-                            Column {
-                                list.forEach {
-                                    Box(modifier = Modifier.fillMaxWidth()) {
-                                        Text(
-                                            modifier = Modifier.align(Alignment.TopStart),
-                                            text = it.first
-                                        )
-                                        Text(
-                                            modifier = Modifier.align(Alignment.TopEnd),
-                                            text = it.second.toString()
-                                        )
-                                    }
-                                }
-                            }
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colors.surface
+                        ) {
+                            Text(text = "Options:")
                         }
                     }
                     item {
                         BaseDashboardCard {
                             Column {
-                                Text(text = "Clock:")
-                                Column(modifier = Modifier.padding(start = 8.dp)) {
-                                    Row {
-                                        Text("Actions:")
-                                        OutlinedButton(
-                                            onClick = {
-                                                intend.modifyRemainingTimeByAmount(5000)
-                                            }
-                                        ) {
-                                            Text("+5s")
+                                Row {
+                                    Text("DurationMillis: ")
+                                    Text(text = intend.typingOptions.durationMillis.toString())
+                                }
+                                Row {
+                                    Text("Type: ")
+                                    Text(text = intend.typingOptions.type.toString())
+                                }
+                            }
+                        }
+                    }
+                    stickyHeader {
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colors.surface
+                        ) {
+                            Text(text = "Clock:")
+                        }
+                    }
+                    item {
+                        BaseDashboardCard {
+                            Column {
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                                ) {
+                                    Text("Actions:")
+                                    OutlinedButton(
+                                        onClick = {
+                                            intend.modifyRemainingTimeByAmount(5000)
                                         }
-                                        OutlinedButton(
-                                            onClick = {
-                                                intend.modifyRemainingTimeByAmount(-5000)
-                                            }
-                                        ) {
-                                            Text("-5s")
+                                    ) {
+                                        Text("+5s")
+                                    }
+                                    OutlinedButton(
+                                        onClick = {
+                                            intend.modifyRemainingTimeByAmount(-5000)
                                         }
+                                    ) {
+                                        Text("-5s")
                                     }
-                                    Row {
-                                        val clock = intend.typingClockStateStateFlow.collectAsState()
-                                        Text("State: ")
-                                        Text(text = clock.value.toString())
-                                    }
+                                }
+                                Row {
+                                    val clock = intend.typingClockStateStateFlow.collectAsState()
+                                    Text("State: ")
+                                    Text(text = clock.value.toString())
+                                }
 
-                                    val timer = intend.timerStateFlow.collectAsState()
+                                val timer = intend.timerStateFlow.collectAsState()
+                                Row {
+                                    Text("Time: ")
+                                    Text(text = timer.value.toString())
+                                }
+                                val timerUpdateCycles = intend.timerUpdateCycleCountStateFlow.collectAsState()
+                                val timeRem = (intend.typingOptions.durationMillis - timer.value).coerceAtLeast(1)
+                                Column {
                                     Row {
-                                        Text("Time: ")
-                                        Text(text = timer.value.toString())
+                                        Text("Update Cycles: ")
+                                        Text(text = timerUpdateCycles.value.toString())
                                     }
-                                    val timerUpdateCycles = intend.timerUpdateCycleCountStateFlow.collectAsState()
-                                    val timeRem = (intend.typingOptions.durationMillis - timer.value).coerceAtLeast(1)
-                                    Column {
-                                        Row {
-                                            Text("Update Cycles: ")
-                                            Text(text = timerUpdateCycles.value.toString())
+                                    Text(text = "Average Cycles:" + ("".takeUnless { intend.timeSkip.value }
+                                        ?: " [Values not accurate due to time skip]"))
+                                    Column(modifier = Modifier.padding(start = 8.dp)) {
+                                        Row(
+                                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                                        ) {
+                                            Text(text = "cycles / ms:")
+                                            Text(
+                                                text = timerUpdateCycles.value.div(timeRem).toString()
+                                            )
                                         }
-                                        Text(text = "Average Cycles:" + ("".takeUnless { intend.timeSkip.value } ?: " [Values not accurate due to time skip]"))
-                                        Column(modifier = Modifier.padding(start = 8.dp)) {
-                                            Row(
-                                                horizontalArrangement = Arrangement.spacedBy(8.dp)
-                                            ) {
-                                                Text(text = "cycles / ms:")
-                                                Text(
-                                                    text = timerUpdateCycles.value.div(timeRem).toString()
-                                                )
-                                            }
-                                            Row(
-                                                horizontalArrangement = Arrangement.spacedBy(8.dp)
-                                            ) {
-                                                Text(text = "cycles / ns:")
-                                                Text(
-                                                    text = timerUpdateCycles.value.div(timeRem)
-                                                        .div(1000f).toString()
-                                                )
-                                            }
+                                        Row(
+                                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                                        ) {
+                                            Text(text = "cycles / ns:")
+                                            Text(
+                                                text = timerUpdateCycles.value.div(timeRem)
+                                                    .div(1000f).toString()
+                                            )
                                         }
                                     }
                                 }
