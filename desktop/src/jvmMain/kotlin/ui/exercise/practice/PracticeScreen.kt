77b6c37a063ce65334ea7cf168d6ccfeec39a739
mzumtobel <36735214+mzumtobel@users.noreply.github.com>
2021-03-07 18:11:48 +0100

    initial commit - added expanding card for video feed

--- desktop/src/jvmMain/kotlin/ui/exercise/practice/PracticeScreen.kt
+++ desktop/src/jvmMain/kotlin/ui/exercise/practice/PracticeScreen.kt
@@ -7,23 +7,23 @@ import androidx.compose.desktop.AppWindow
 import androidx.compose.desktop.LocalAppWindow
 import androidx.compose.desktop.Window
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
@@ -67,23 +67,98 @@ private fun PracticeScreenContent(intend: IPracticeIntend) {
         }
     }
     val max = intend.typingOptions.durationMillis.div(1000).toFloat()
-    Column(
-        modifier = Modifier.fillMaxWidth(0.8f)
-    ) {
-        val timer = intend.timerStateFlow.collectAsState()
-        CountDownProgressBar(
-            modifier = Modifier.fillMaxWidth(),
-            value = max - timer.value.div(1000).toFloat(),
-            max = max
-        )
-        Text(text = "Generated text:")
-        val text = intend.textStateFlow.collectAsState()
-        Text(text = text.value)
-        Button(
-            onClick = intend::start
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    )
+    {
+        Column (
+            modifier = Modifier.fillMaxWidth().align(Alignment.Center).padding(25.dp)
         ) {
-            Text("Start Timer")
+            //progress bar
+                val timer = intend.timerStateFlow.collectAsState()
+                CountDownProgressBar(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = max - timer.value.div(1000).toFloat(),
+                    max = max
+                )
+            // text in base dashboard card
+            // TODO adjust to fit generated text -> all test on screen or just one row etc.
+                BaseDashboardCard(
+                    modifier = Modifier
+                        .fillMaxWidth(0.7f)
+                        .padding(vertical = 8.dp)
+                        .fillMaxHeight(0.5f)
+                ) {
+                    Column (
+                        modifier = Modifier.align(Alignment.Center).fillMaxSize()
+                            ){
+                        Row(
+                            modifier = Modifier.fillMaxWidth()
+                                .padding(50.dp),
+                        )
+                        {
+                            Text(text = "Generated text:")
+                            val text = intend.textStateFlow.collectAsState()
+                            Text(text = text.value)
+                        }
+                    }
+                }
+            // button to start progress bar
+                Button(
+                    onClick = intend::start
+                ) {
+                    Text("Start Timer")
+                }
+            // collapsable video feed
+                VideoFeedExpanding("Video Feed") //}
         }
+    }
+}
+
+/**
+ * Expanding Card fo collapsable Video feed
+ * TODO
+ */
+@Composable
+private fun VideoFeedExpanding(
+    title:String,
+    bgColor: Color = MaterialTheme.colors.background,
+    shape: Shape = RoundedCornerShape(16.dp) //TODO change to fit bottom of screen
+){
+    var expanded by remember { mutableStateOf(false) }
+    Card(
+        //modifier = Modifier.padding(50.dp).fillMaxWidth()
+        shape = shape,
+        backgroundColor = bgColor,
+        elevation = 16.dp
+    ){
+        Column {
+            Text(text = title, textAlign = TextAlign.Center)
+            if(expanded) {
+                //Text(text = body)
+                VideoFeedLive()
+                IconButton(onClick = { expanded = false }) {
+                    Icon(Icons.Default.ExpandLess, contentDescription = "Collapse")
+                }
+            } else {
+                IconButton(onClick = {expanded = true}) {
+                    Icon(Icons.Default.ExpandMore, contentDescription = "Expand")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun VideoFeedLive(){ //placeholder for actual live feed
+    Box(
+        modifier = Modifier.fillMaxWidth().padding(50.dp)
+            .background(Color.DarkGray)
+    ) {
+        Text(modifier = Modifier.padding(50.dp),
+        text = "Live Feed Placeholder",
+        color = Color.LightGray)
     }
 }
 
