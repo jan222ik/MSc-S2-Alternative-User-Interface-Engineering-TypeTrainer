927bee592b7d4ee8c4e979e34fd1253b16f33862
Matthias <mle2266@students.fhv.at>
2021-03-22 14:10:20 +0100

    Continued ResultsOverview UI

--- desktop/src/jvmMain/kotlin/ui/exercise/results/ResultsOverview.kt
+++ desktop/src/jvmMain/kotlin/ui/exercise/results/ResultsOverview.kt
@@ -2,16 +2,22 @@
 
 package ui.exercise.results
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ui.dashboard.BaseDashboardCard
@@ -53,21 +59,35 @@ fun KeyPoints(modifier: Modifier, keyPoints: List<KeyPoint>) {
     ) {
         Column {
             Text(text = +RequiresTranslationI18N("KeyPoints") + ":")
-            LazyColumn {
-
-                items(keyPoints) { keyPoint ->
-                    Row(modifier = Modifier.fillMaxWidth()) {
+            Row {
+                LazyColumn(
+                    contentPadding = PaddingValues(end = 15.dp),
+                    horizontalAlignment = Alignment.End
+                ) {
+                    items(keyPoints) { keyPoint ->
                         Text(
                             text = keyPoint.name + ":",
                             color = MaterialTheme.colors.primary,
-                            modifier = Modifier.weight(1f)
+                            modifier = Modifier,
+                            textAlign = TextAlign.Justify
                         )
-                        Row(modifier = Modifier.weight(1f)) {
-                            Text(text = keyPoint.value)
+                    }
+                }
+                LazyColumn {
+                    items(keyPoints) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(5.dp)
+                        ) {
+                            Text(
+                                text = it.value,
+                                textAlign = TextAlign.Justify
+                            )
                             Icon(
-                                imageVector = keyPoint.icon,
+                                imageVector = it.icon,
                                 contentDescription = "Icon",
-                                tint = MaterialTheme.colors.primary
+                                tint = MaterialTheme.colors.primary,
+                                modifier = Modifier.size(15.dp)
                             )
                         }
                     }
@@ -76,6 +96,7 @@ fun KeyPoints(modifier: Modifier, keyPoints: List<KeyPoint>) {
         }
     }
 }
+
 
 @Composable
 fun Goals(modifier: Modifier) {
@@ -91,27 +112,47 @@ fun Achievements(modifier: Modifier, achievements: List<List<String>>) {
     BaseDashboardCard(
         modifier = modifier
     ) {
-        Column {
-            Text(text = +RequiresTranslationI18N("Achievements") + ":")
-            LazyColumn {
-                items(achievements) { achievement ->
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        Text(
-                            text = achievement.first(),
-                            fontStyle = FontStyle.Italic,
-                            fontSize = 15.sp,
-                            modifier = Modifier.align(Alignment.Start)
-                        )
-                        Text(
-                            text = achievement.last(),
-                            color = MaterialTheme.colors.primary,
-                            modifier = Modifier.align(Alignment.End)
-                        )
-                    }
+        Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
+            Row(horizontalArrangement = Arrangement.spacedBy(15.dp)) {
+                Text(text = +RequiresTranslationI18N("Achievements") + ":")
+                val color = MaterialTheme.colors.primary
+                Text(text = achievements.size.toString(), modifier = Modifier.drawBehind {
+                    drawCircle(color = color, radius = 13f)
+                })
 
+            }
+            LazyColumn(
+                contentPadding = PaddingValues(top = 5.dp),
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.End
+            ) {
+                items(achievements) { achievement ->
+                    Card(
+                        backgroundColor = MaterialTheme.colors.background,
+                        border = BorderStroke(width = 2.dp, color = MaterialTheme.colors.primary),
+                        shape = RoundedCornerShape(16.dp),
+                        modifier = Modifier.fillMaxWidth(0.85f).padding(top = 5.dp),
+                        ) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier.fillMaxWidth().padding(10.dp),
+                        ) {
+                            Text(
+                                text = achievement.first(),
+                                fontStyle = FontStyle.Italic,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 15.sp,
+                                modifier = Modifier.align(Alignment.Start)
+                            )
+                            Text(
+                                text = achievement.last(),
+                                color = MaterialTheme.colors.primary,
+                                modifier = Modifier.align(Alignment.End)
+                            )
+                        }
+                    }
                 }
             }
         }
-
     }
 }
