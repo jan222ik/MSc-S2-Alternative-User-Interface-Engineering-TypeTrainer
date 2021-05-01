bcb7d46e076d4006ace8869b89129b9562d5b64c
Janik Mayr <32466915+jan222ik@users.noreply.github.com>
2021-05-02 00:42:13 +0200

    Added create new goal button for dashboard

--- desktop/src/jvmMain/kotlin/ui/dashboard/goal_preview/GoalPreview.kt
+++ desktop/src/jvmMain/kotlin/ui/dashboard/goal_preview/GoalPreview.kt
@@ -2,18 +2,19 @@
 
 package ui.dashboard.goal_preview
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.TargetBasedAnimation
 import androidx.compose.animation.core.VectorConverter
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.core.updateTransition
 import androidx.compose.desktop.Window
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -23,26 +24,26 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Assignment
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.withFrameMillis
 import androidx.compose.runtime.withFrameNanos
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerMoveFilter
 import androidx.compose.ui.unit.dp
 import com.github.jan222ik.common.ui.components.TypeTrainerTheme
 import com.github.jan222ik.common.ui.dashboard.BaseDashboardCard
 import kotlinx.coroutines.launch
+import ui.dashboard.ApplicationRoutes
+import ui.general.WindowRouterAmbient
 import ui.util.i18n.LanguageConfiguration
 import ui.util.i18n.RequiresTranslationI18N
 
@@ -53,13 +54,18 @@ fun DashboardGoalsPreviewCard(goalIntend: GoalIntend = GoalIntend()) {
             Box(
                 modifier = Modifier.fillMaxWidth()
             ) {
-                Row(modifier = Modifier.align(Alignment.CenterStart)) {
+                Row(
+                    modifier = Modifier.align(Alignment.CenterStart),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(5.dp)
+                ) {
                     Icon(
                         imageVector = Icons.Filled.Assignment,
                         contentDescription = null
                     )
                     Text(
-                        text = +RequiresTranslationI18N("Goals: [Placeholder]")
+                        text = +RequiresTranslationI18N("Goals: [Placeholder]"),
+                        style = MaterialTheme.typography.h5
                     )
                 }
                 TextButton(
@@ -70,11 +76,58 @@ fun DashboardGoalsPreviewCard(goalIntend: GoalIntend = GoalIntend()) {
                     }
                 )
             }
-            Row {
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
                 val (first, second) = goalIntend.getPreviewGoals()
                 DashboardGoal(first, {})
                 DashboardGoal(second, {})
+                ComposeNewGoalBtn()
             }
+        }
+    }
+}
+
+@Composable
+fun ComposeNewGoalBtn() {
+    val router = WindowRouterAmbient.current
+    val onHover = remember { mutableStateOf(false) }
+    val color = animateColorAsState(when(onHover.value) {
+        true -> MaterialTheme.colors.primary
+        false -> MaterialTheme.colors.onBackground
+    })
+    Box(
+        modifier = Modifier
+            .pointerMoveFilter(
+                onEnter = {
+                    onHover.component2()(true)
+                    true
+                },
+                onExit = {
+                    onHover.component2()(false)
+                    true
+                }
+            )
+            .clickable(
+                onClick = {
+                    router.navTo(ApplicationRoutes.Goals.Compose)
+                }
+            )
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Add,
+                contentDescription = null,
+                tint = color.value
+            )
+            Text(
+                text = +RequiresTranslationI18N("Compose a new goal"),
+                color = color.value
+            )
         }
     }
 }
@@ -145,7 +198,7 @@ fun DashboardGoal(goal: IGoal, onOpenGoal: (IGoal) -> Unit) {
                         drawArc(
                             color = activeTrackColor,
                             startAngle = -90f,
-                            sweepAngle =  animationValue,
+                            sweepAngle = animationValue,
                             useCenter = false,
                             style = if (isHover) hoverStrokeStyle else strokeStyle
                         )
