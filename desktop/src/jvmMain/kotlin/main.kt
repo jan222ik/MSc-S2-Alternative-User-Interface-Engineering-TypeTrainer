def3183d08de6902857da6c7081212e425463018
Matthias Lerch <matthias.lerch@students.fhv.at>
2021-06-17 08:47:32 +0200

    prototype of camera setup screen

--- desktop/src/jvmMain/kotlin/main.kt
+++ desktop/src/jvmMain/kotlin/main.kt
@@ -1,9 +1,11 @@
 @file:Suppress("FunctionName")
 
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import io.ktor.util.KtorExperimentalAPI
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
+@ExperimentalAnimationApi
 @KtorExperimentalAPI
 @ExperimentalCoroutinesApi
 @ExperimentalStdlibApi
